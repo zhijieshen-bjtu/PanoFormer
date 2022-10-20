38a18085bd2aac02884c44438cdbaaff83ad5932
Zhijie Shen <48438062+zhijieshen-bjtu@users.noreply.github.com>
2022-10-20 14:56:53 +0800

    Delete PanoFormer directory

--- PanoFormer/network/testpoint.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-
-ref_point2x4 = torch.load("Equioffset2x4.pth")
-
-print(ref_point2x4)
--- PanoFormer/newmetrics/py360convert/__init__.py
+++
@@ -1,4 +0,0 @@
-from .e2c import e2c
-from .e2p import e2p
-from .c2e import c2e
-from .utils import *
